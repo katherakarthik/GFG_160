3f4d45a8814218bfcd6d7d375f9c6a77594ab4db
karthikkathera <120442160+katherakarthik@users.noreply.github.com>
2025-07-24 10:51:21 +0530

    Create Stock_BuySell_Multiple_transactions_allowed.java

---
+++ Stock_BuySell_Multiple_transactions_allowed.java
@@ -0,0 +1,15 @@
+class GfG {
+    
+    static int maximumProfit(int[] prices) {
+        int res = 0;
+
+        // Keep on adding the difference between
+        // adjacent when the prices a
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) 
+                res += prices[i] - prices[i - 1];
+        }
+        
+        return res;
+    }
+}
